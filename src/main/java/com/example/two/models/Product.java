8118da305b6df2b566a53788fddfe38f387e9140
wijnandK <korlaarwijnand@hotmail.com>
2022-08-25 15:03:55 +0200

    Setup controllers

---
+++ src/main/java/com/example/two/models/Product.java
@@ -0,0 +1,64 @@
+package com.example.two.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+//import com.webtutsplus.ecommerce.dto.product.ProductDto;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private @NotNull String name;
+
+    private @NotNull String description;
+
+
+    public Product(Integer id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Product() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+
+                '}';
+    }
+}
---
+++ src/main/java/com/example/two/service/TestService.java
@@ -0,0 +1,17 @@
+package com.example.two.service;
+
+import com.example.two.repositories.TestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+    @Autowired
+    private TestRepository testRepository;
+
+
+
+//        public getUser()
+
+
+}
